880b06ace8257fff958cd52a826554032e6f214c
Ayush kashyap <132879547+AyushK9140@users.noreply.github.com>
2024-06-13 06:48:00 +0000

    basics how variable declared

---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,8 @@
+const accountId=2003
+var accountName="Ayush"
+let accountAddress="Lucknow"
+accountState="UttarPradesh"
+
+// console.log(accountId) // to print individual variable's value
+
+console.table([accountId,accountName,accountAddress,accountState])

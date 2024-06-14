fbadbd77d330c02ff0215a3c56df7e83a868d11a
Ayush kashyap <132879547+AyushK9140@users.noreply.github.com>
2024-06-14 05:06:31 +0000

    changes config done

---
+++ .devcontainer/basics/operation.js
@@ -0,0 +1,20 @@
+//Basic operations
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(2**2)
+
+//Addition of sting
+
+let str1="Ayush"
+let str2=" Kashyap"
+let str3=str1+str2
+console.log(str3)
+
+//some confusing operations  ->  this is not the good way to write the code so always write the code using pararnteses to  make code more readable and understandable
+console.log("1" + 2)
+console.log(1+1+"2")  // before reading string value will be added
+console.log(1+"2"+3)
+

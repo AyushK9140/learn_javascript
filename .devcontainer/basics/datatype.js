6fa9b1ee4ff2bde826614ee17415f05ceca163a2
Ayush kashyap <132879547+AyushK9140@users.noreply.github.com>
2024-06-15 04:56:27 +0000

    changes configure done

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,14 @@
+//datatype of null --> ask in mant interviews
+let score=null;
+console.log(typeof(score)) // object
+
+
+let biInt=122322333.6
+console.log(typeof(bigInt)) //lets check the ans ->> undefined
+
+let name="Ayush" //string
+
+let age=20 //Number
+
+
+console.table([typeof(score),typeof(bigInt),typeof(name),typeof(age)]) // printing all the value in a single table

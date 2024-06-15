acd52fd21edbd821a01291d8f1c37446a9f0adb1
Ayush kashyap <132879547+AyushK9140@users.noreply.github.com>
2024-06-15 06:08:12 +0000

    changes config done

---
+++ .devcontainer/basics/string.js
@@ -0,0 +1,19 @@
+// method 1to declare string
+const name="Ayush"
+console.log(name)
+
+//another method to declare string by creating object
+ const firstname=new String('   Ayush    ') // this method is called string interpolation
+ console.log(firstname)
+
+ //methods in string
+
+ console.log(name.lenght)
+ console.log(name.indexOf('y'))
+ console.log(name.charAt(3))
+ console.log(name.substring(0,4))
+ console.log(name.replace('y' , 'k'))
+ console.log(firstname.trim())
+ console.log(firstname.trimStart())
+ console.log(name.slice(-4,-2))
+ console.log(firstname.trimEnd())

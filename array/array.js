3fb4cd50a6a66df3667c36711516d4c62a5cb7fd
Ayush kashyap <132879547+AyushK9140@users.noreply.github.com>
2024-06-17 15:19:29 +0000

    changes configure done

---
+++ array/array.js
@@ -0,0 +1,23 @@
+// declaration of Array
+
+const objArr=[0,1,2,3,4]
+const newArr=[1,2,3,"Ayush"]
+const myArr=new Array(1,2,3,4,5)
+
+console.table([myArr,newArr,objArr])
+
+// Methods in Array
+
+console.log(myArr.push(6))
+console.log(myArr)
+console.log(myArr.indexOf(3))
+console.log(myArr.pop())
+console.log(myArr)
+console.log(myArr.unshift(9))
+console.log(myArr)
+console.log(myArr.shift())
+console.log(myArr)
+console.log(myArr.slice(0,3))
+console.log(myArr)
+console.log(myArr.splice(0,3))
+console.log(myArr)

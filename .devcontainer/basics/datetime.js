dd03ff9a1775478364a2a0aabf268f4e2280b5f3
Ayush kashyap <132879547+AyushK9140@users.noreply.github.com>
2024-06-17 14:32:00 +0000

    changes configure done

---
+++ .devcontainer/basics/datetime.js
@@ -0,0 +1,9 @@
+let mydate=new Date()
+console.log(mydate.toString()) // Mon Jun 17 2024 14:24:54 GMT+0000 (Coordinated Universal Time)--> current date and time
+
+// if We want to declare date 
+
+let date=new Date(5-12-2024)
+console.log(date.toString())
+
+console.log(date.toDateString())//Wed Dec 31 1969
